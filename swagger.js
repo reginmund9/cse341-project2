137b0af6dfd6389701172758ec88b7aa553ae7be
reginmund9 <moronitaca@gmail.com>
2024-07-02 05:09:02 -0600

    faith

--- swagger.js
+++ swagger.js
@@ -1,15 +1,21 @@
+
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-    info: {
-        title: 'Users Api',
-        description: 'Users Api'
-    },
-    host: 'localhost: 3001',
-    schemes: ['http', 'https'] 
- };
+  info: {
+    title: 'Users Api',
+    description: 'Users Api'
+  },
+  host: 'project2-dgm3.onrender.com',
+  schemes: [
+    'https'
+]
+};
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFile = ['./routes/index.js'];
 
-swaggerAutogen(outputFile, endpointsFiles, doc)
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, endpointsFile, doc);
